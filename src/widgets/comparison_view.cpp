7445ebbed244d12c6dad5b0ab22d7414f1192c74
Daniel Saier <mail@danielsaier.de>
2021-08-22 17:40:22 +0200

    Refactor the computation of comparison images

--- src/widgets/comparison_view.cpp
+++ src/widgets/comparison_view.cpp
@@ -9,6 +9,81 @@
 
 #include <cmath>
 
+namespace {
+
+struct ComparisonViewLayer {
+    QPixmap pixmap;
+    double opacity = 1.;
+};
+
+using ComparisonViewLayers = std::vector<ComparisonViewLayer>;
+
+ComparisonViewLayers computeHighlightDifferencesLayers(std::vector<Session::ImageHandle> const& images,
+                                                       ComparisonSettings const& settings)
+{
+    if (images.empty()) {
+        return {};
+    }
+    if (images.size() < 2) {
+        return {{images[0]->toGrayscalePixmap()}};
+    }
+
+    QImage differenceImage;
+    std::vector<QImage> imagesToCompare;
+    std::transform(images.begin(), images.end(), std::back_inserter(imagesToCompare), [](auto const& image) {
+        return image->image();
+    });
+
+    differenceImage =
+        computeDifferenceImage(imagesToCompare, settings.getDifferenceTolerance(), settings.showMinorDifferences());
+
+    return {
+        {images[0]->toGrayscalePixmap()},
+        {QPixmap::fromImage(differenceImage), 0.7},
+    };
+}
+
+ComparisonViewLayers computeBlendLayers(std::vector<Session::ImageHandle> const& images,
+                                        ComparisonSettings const& settings)
+{
+    if (images.size() < 2) {
+        return {};
+    }
+
+    double const sequenceBlendPosition = settings.getBlendPosition() * static_cast<double>(images.size() - 1);
+
+    auto firstImageIndex = static_cast<size_t>(std::floor(sequenceBlendPosition));
+    firstImageIndex = std::min(firstImageIndex, images.size() - 2);
+
+    double const blendPosition = sequenceBlendPosition - firstImageIndex;
+
+    QColor firstImageColor = settings.blendImage1Color();
+    QColor secondImageColor = settings.blendImage2Color();
+    if (firstImageIndex % 2 != 0) {
+        std::swap(firstImageColor, secondImageColor);
+    }
+
+    return {
+        {images[firstImageIndex]->toColorizedPixmap(firstImageColor), 1 - blendPosition},
+        {images[firstImageIndex + 1]->toColorizedPixmap(secondImageColor), blendPosition},
+    };
+}
+
+ComparisonViewLayers computeComparisonLayers(std::vector<Session::ImageHandle> const& images,
+                                             ComparisonSettings const& settings)
+{
+    switch (settings.getComparisonMode()) {
+        case ComparisonMode::HighlightDifferences:
+            return computeHighlightDifferencesLayers(images, settings);
+        case ComparisonMode::BlendImages:
+            return computeBlendLayers(images, settings);
+    }
+
+    return {};
+}
+
+}  // namespace
+
 ComparisonView::ComparisonView(QWidget* parent)
   : ImageView(parent)
 {
@@ -22,43 +97,8 @@ void ComparisonView::update(Session const& session, ComparisonSettings const& se
 
     clear();
 
-    auto const& images = session.getImages();
-
-    ComparisonMode comparisonMode = settings.getComparisonMode();
-    if (comparisonMode == ComparisonMode::HighlightDifferences) {
-        QImage differenceImage;
-        if (images.size() >= 2) {
-            std::vector<QImage> imagesToCompare;
-            imagesToCompare.reserve(images.size());
-            for (auto const& image : images) {
-                imagesToCompare.push_back(image->image());
-            }
-
-            differenceImage = computeDifferenceImage(imagesToCompare,
-                                                     settings.getDifferenceTolerance(),
-                                                     settings.showMinorDifferences());
-        }
-
-        addPixmap(images[0]->toGrayscalePixmap());
-        addPixmap(QPixmap::fromImage(differenceImage), 0.7);
-    } else if (comparisonMode == ComparisonMode::BlendImages && images.size() >= 2) {
-        double const sequenceBlendPosition = settings.getBlendPosition() * static_cast<double>(images.size() - 1);
-
-        auto firstImageIndex = static_cast<size_t>(std::floor(sequenceBlendPosition));
-        firstImageIndex = std::min(firstImageIndex, images.size() - 2);
-
-        double const blendPosition = sequenceBlendPosition - firstImageIndex;
-
-        QColor firstImageColor = settings.blendImage1Color();
-        QColor secondImageColor = settings.blendImage2Color();
-        if (firstImageIndex % 2 != 0) {
-            std::swap(firstImageColor, secondImageColor);
-        }
-
-        auto* firstImage = getScene().addPixmap(images[firstImageIndex]->toColorizedPixmap(firstImageColor));
-        firstImage->setOpacity(1 - blendPosition);
-        auto* secondImage = getScene().addPixmap(images[firstImageIndex + 1]->toColorizedPixmap(secondImageColor));
-        secondImage->setOpacity(blendPosition);
+    for (auto const& layer : computeComparisonLayers(session.getImages(), settings)) {
+        addPixmap(layer.pixmap, layer.opacity);
     }
 
     updateSceneRect();
