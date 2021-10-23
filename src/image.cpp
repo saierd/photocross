saierd/photocross

commit 7729429d1e823c543b9b27717cf3c5635ce7880e

Cache transformed versions of images
Since images can now be offset, rotated and colorized, updating the
views can take quite some time to recompute these transformed versions
of the original images. This is not necessary when e.g. only the opacity
ratio of a blending comparison changes. We therefore cache transformed
images until a parameter changes that actually requires them to be
recomputed.

src/image.cpp
@@ -2,13 +2,44 @@
 
 #include "image_helpers.h"
 
+struct Image::Cache {
+    QImage image;
+    QImage grayscale;
+
+    QPixmap pixmap;
+    QPixmap grayscalePixmap;
+
+    QColor colorizedPixmapColor;
+    QPixmap colorizedPixmap;
+
+    void invalidate()
+    {
+        *this = Cache();
+    }
+};
+
 Image::Image(QString _filename)
   : filename(std::move(_filename))
   , imageData(filename)
 {
     fileWatcher.addPath(filename);
     connect(&fileWatcher, &QFileSystemWatcher::fileChanged, this, &Image::imageFileChanged);
+
+    // We will save converted versions of the image in the cache to avoid recomputing them whenever the display of the
+    // image in the UI changes.
+    cache = std::make_unique<Cache>();
+
+    // Clear the cache when the image changes. Note that this must be the first slot that gets connected to the
+    // imageChanged signal so that any other slots reacting to the event can get refreshed images. Qt guarantees that
+    // slots are called in the order they are connected.
+    connect(this, &Image::imageChanged, [this]() {
+        if (cache) {
+            cache->invalidate();
+        }
+    });
 }
+
+Image::~Image() = default;
 
 QString const& Image::file() const&
 {
@@ -17,6 +48,10 @@ QString const& Image::file() const&
 
 QImage Image::image() const&
 {
+    if (!cache->image.isNull()) {
+        return cache->image;
+    }
+
     QImage rotated;
     if (rotation != 0) {
         // Note that rotation by positive angles is clockwise here since Qt widget coordinates have their y axis
@@ -26,11 +61,16 @@ QImage Image::image() const&
         rotated = imageData;
     }
 
-    return addTransparentOffset(rotated, offset);
+    cache->image = addTransparentOffset(rotated, offset);
+    return cache->image;
 }
 
 QImage Image::toGrayscaleImage() const
 {
+    if (!cache->grayscale.isNull()) {
+        return cache->grayscale;
+    }
+
     QImage originalImage = image();
     QImage grayscale = originalImage.convertToFormat(QImage::Format_Grayscale8);
     if (originalImage.hasAlphaChannel()) {
@@ -38,22 +78,37 @@ QImage Image::toGrayscaleImage() const
         grayscale.setAlphaChannel(originalImage.convertToFormat(QImage::Format_Alpha8));
     }
 
-    return grayscale;
+    cache->grayscale = grayscale;
+    return cache->grayscale;
 }
 
 QPixmap Image::toPixmap() const
 {
-    return QPixmap::fromImage(image());
+    if (cache->pixmap.isNull()) {
+        cache->pixmap = QPixmap::fromImage(image());
+    }
+    return cache->pixmap;
 }
 
 QPixmap Image::toGrayscalePixmap() const
 {
-    return QPixmap::fromImage(toGrayscaleImage());
+    if (cache->pixmap.isNull()) {
+        cache->pixmap = QPixmap::fromImage(toGrayscaleImage());
+    }
+    return cache->pixmap;
 }
 
 QPixmap Image::toColorizedPixmap(QColor const& color) const
 {
-    return QPixmap::fromImage(colorizeGrayscaleImage(toGrayscaleImage(), color));
+    if (!cache->colorizedPixmap.isNull() && cache->colorizedPixmapColor == color) {
+        return cache->colorizedPixmap;
+    }
+
+    auto colorized = QPixmap::fromImage(colorizeGrayscaleImage(toGrayscaleImage(), color));
+    cache->colorizedPixmap = colorized;
+    cache->colorizedPixmapColor = color;
+
+    return cache->colorizedPixmap;
 }
 
 void Image::setReloadWhenFileChanges(bool enable)

src/image.h
@@ -5,11 +5,14 @@
 #include <QPixmap>
 #include <QStringList>
 
+#include <memory>
+
 class Image : public QObject {
     Q_OBJECT
 
 public:
     explicit Image(QString _filename);
+    ~Image() override;
 
     QString const& file() const&;
     QImage image() const&;
@@ -49,4 +52,7 @@ private:
 
     bool reloadWhenFileChanges = false;
     QFileSystemWatcher fileWatcher;
+
+    struct Cache;
+    std::unique_ptr<Cache> cache;
 };

commit 7c1ce1b5cd93b4fa53f247a5c2e52ec7bba5d9ad

Fix caching of grayscale pixmaps
Grayscale pixmaps overrode the normal images. Broken since
7729429d1e823c543b9b27717cf3c5635ce7880e.

src/image.cpp
@@ -92,10 +92,10 @@ QPixmap Image::toPixmap() const
 
 QPixmap Image::toGrayscalePixmap() const
 {
-    if (cache->pixmap.isNull()) {
-        cache->pixmap = QPixmap::fromImage(toGrayscaleImage());
+    if (cache->grayscalePixmap.isNull()) {
+        cache->grayscalePixmap = QPixmap::fromImage(toGrayscaleImage());
     }
-    return cache->pixmap;
+    return cache->grayscalePixmap;
 }
 
 QPixmap Image::toColorizedPixmap(QColor const& color) const