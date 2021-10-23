saierd/photocross

commit 29abf4fb347a5a77a253dd65457fd70917dad7fc

Precompute colorized images instead of using QGraphicsColorizeEffect
The effect is executed every time when the graphics view redraws. It
therefore made moving and zooming the images very slow for large
colorized images.

src/image.cpp
@@ -29,12 +29,7 @@ QImage Image::image() const&
     return addTransparentOffset(rotated, offset);
 }
 
-QPixmap Image::toPixmap() const
-{
-    return QPixmap::fromImage(image());
-}
-
-QPixmap Image::toGrayscalePixmap() const
+QImage Image::toGrayscaleImage() const
 {
     QImage originalImage = image();
     QImage grayscale = originalImage.convertToFormat(QImage::Format_Grayscale8);
@@ -43,7 +38,22 @@ QPixmap Image::toGrayscalePixmap() const
         grayscale.setAlphaChannel(originalImage.convertToFormat(QImage::Format_Alpha8));
     }
 
-    return QPixmap::fromImage(grayscale);
+    return grayscale;
+}
+
+QPixmap Image::toPixmap() const
+{
+    return QPixmap::fromImage(image());
+}
+
+QPixmap Image::toGrayscalePixmap() const
+{
+    return QPixmap::fromImage(toGrayscaleImage());
+}
+
+QPixmap Image::toColorizedPixmap(QColor const& color) const
+{
+    return QPixmap::fromImage(colorizeGrayscaleImage(toGrayscaleImage(), color));
 }
 
 void Image::setReloadWhenFileChanges(bool enable)

src/image.h
@@ -13,9 +13,11 @@ public:
 
     QString const& file() const&;
     QImage image() const&;
+    QImage toGrayscaleImage() const;
 
     QPixmap toPixmap() const;
     QPixmap toGrayscalePixmap() const;
+    QPixmap toColorizedPixmap(QColor const& color) const;
 
     void setReloadWhenFileChanges(bool enable);
 

src/image_helpers.cpp
@@ -16,3 +16,23 @@ QImage addTransparentOffset(QImage const& image, QPoint const& offset)
 
     return newImage;
 }
+
+QImage colorizeGrayscaleImage(QImage const& image, QColor const& color)
+{
+    if (!color.isValid()) {
+        return image;
+    }
+
+    QImage target = image.convertToFormat(QImage::Format_ARGB32);
+
+    QPainter painter(&target);
+    painter.setCompositionMode(QPainter::CompositionMode_Screen);
+    painter.fillRect(image.rect(), color);
+    painter.end();
+
+    if (image.hasAlphaChannel()) {
+        target.setAlphaChannel(image.alphaChannel());
+    }
+
+    return target;
+}

src/image_helpers.h
@@ -3,3 +3,5 @@
 #include <QImage>
 
 QImage addTransparentOffset(QImage const& image, QPoint const& offset);
+
+QImage colorizeGrayscaleImage(QImage const& image, QColor const& color);

src/widgets/comparison_view.cpp
@@ -15,17 +15,6 @@ ComparisonView::ComparisonView(QWidget* parent)
     setCaption("Comparison View");
 }
 
-void addColorFilter(QGraphicsPixmapItem* item, QColor const& color)
-{
-    if (!color.isValid()) {
-        return;
-    }
-
-    auto effect = std::make_unique<QGraphicsColorizeEffect>();
-    effect->setColor(color);
-    item->setGraphicsEffect(effect.release());
-}
-
 void ComparisonView::update(Session const& session, ComparisonSettings const& settings)
 {
     // Clearing the scene will reset the view position. Store the current view settings and restore it after the update.
@@ -60,19 +49,16 @@ void ComparisonView::update(Session const& session, ComparisonSettings const& se
 
         double const blendPosition = sequenceBlendPosition - firstImageIndex;
 
-        auto* firstImage = getScene().addPixmap(images[firstImageIndex]->toPixmap());
-        firstImage->setOpacity(1 - blendPosition);
-        auto* secondImage = getScene().addPixmap(images[firstImageIndex + 1]->toPixmap());
-        secondImage->setOpacity(blendPosition);
-
         QColor firstImageColor = settings.blendImage1Color();
         QColor secondImageColor = settings.blendImage2Color();
         if (firstImageIndex % 2 != 0) {
             std::swap(firstImageColor, secondImageColor);
         }
 
-        addColorFilter(firstImage, firstImageColor);
-        addColorFilter(secondImage, secondImageColor);
+        auto* firstImage = getScene().addPixmap(images[firstImageIndex]->toColorizedPixmap(firstImageColor));
+        firstImage->setOpacity(1 - blendPosition);
+        auto* secondImage = getScene().addPixmap(images[firstImageIndex + 1]->toColorizedPixmap(secondImageColor));
+        secondImage->setOpacity(blendPosition);
     }
 
     updateSceneRect();

commit 801a6828750006bed67723ad82550e191316b2cc

Fix blending with true colors
True color blending is internally represented by an invalid QColor
value. This special case was not caught and images were always converted
to grayscale before coloring them. Broken in
29abf4fb347a5a77a253dd65457fd70917dad7fc.

src/image.cpp
@@ -100,6 +100,10 @@ QPixmap Image::toGrayscalePixmap() const
 
 QPixmap Image::toColorizedPixmap(QColor const& color) const
 {
+    if (!color.isValid()) {
+        return toPixmap();
+    }
+
     if (!cache->colorizedPixmap.isNull() && cache->colorizedPixmapColor == color) {
         return cache->colorizedPixmap;
     }