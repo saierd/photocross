saierd/photocross

commit 48f6525f9757a2c8dd51fcdaa1cb650d8d4fbd30

Refactor the comparison settings widget
Comparison settings were moved to a struct to simplify future
enhancements like saving and restoring settings.

The widget now uses buttons to choose the mode instead of the previous
interface based on tabs. This makes it easier to add more modes.

Modes were split up further to make useful combinations easier to set up
and eliminate some combinations that do not make sense (e.g. animated
blending with false colors as well as blending with more than two
images).

src/widgets/comparison_view.cpp
@@ -3,20 +3,52 @@
 #include "busy_dialog.h"
 #include "comparison_view_layers.h"
 #include "session.h"
+#include "time_helpers.h"
+
+auto constexpr continuousBlendTimerInterval = 50ms;
 
 ComparisonView::ComparisonView(QWidget* parent)
   : ImageView(parent)
 {
     setCaption("Comparison View");
+
+    connect(&animationUpdateTimer, &QTimer::timeout, this, &ComparisonView::animationUpdate);
 }
 
-void ComparisonView::update(Session const& session, ComparisonSettings const& settings)
+void ComparisonView::update(Session const& session, ComparisonSettings const& _settings)
 {
+    images = session.getImages();
+    settings = _settings;
+    update();
+
+    if (settings.mode != ComparisonMode::BlendImagesAnimated) {
+        if (animationUpdateTimer.isActive()) {
+            animationUpdateTimer.stop();
+        }
+    } else {
+        if (settings.animatedBlending.continuous) {
+            animationUpdateTimer.setInterval(continuousBlendTimerInterval);
+        } else {
+            animationUpdateTimer.setInterval(settings.animatedBlending.timeBetweenImages);
+        }
+
+        if (!animationUpdateTimer.isActive()) {
+            animationStartTime = std::chrono::steady_clock::now();
+            animationUpdateTimer.start();
+        }
+    }
+}
+
+void ComparisonView::update()
+{
+    auto timeSinceAnimationStart = std::chrono::steady_clock::now() - animationStartTime;
+    double animationStep = durationRatio(timeSinceAnimationStart, settings.animatedBlending.timeBetweenImages);
+
     ComparisonViewLayers layers;
     runWithBusyDialog(
         "Updating Comparison View...",
         [&]() {
-            layers = computeComparisonViewLayers(session.getImages(), settings);
+            layers = computeComparisonViewLayers(images, settings, animationStep);
         },
         this);
 
@@ -32,4 +64,9 @@ void ComparisonView::update(Session const& session, ComparisonSettings const& se
 
     restoreView();
     forceViewPropagation();
+}
+
+void ComparisonView::animationUpdate()
+{
+    update();
 }

src/widgets/comparison_view.h
@@ -1,13 +1,29 @@
 #pragma once
 
+#include "comparison_settings.h"
+#include "session.h"
 #include "widgets/image_view.h"
 
-class Session;
-class ComparisonSettings;
+#include <QTimer>
+
+#include <chrono>
 
 class ComparisonView : public ImageView {
+    Q_OBJECT
+
 public:
     explicit ComparisonView(QWidget* parent = nullptr);
 
-    void update(Session const& session, ComparisonSettings const& settings);
+    void update(Session const& session, ComparisonSettings const& _settings);
+
+private slots:
+    void update();
+    void animationUpdate();
+
+private:
+    Session::Images images;
+    ComparisonSettings settings;
+
+    QTimer animationUpdateTimer;
+    std::chrono::steady_clock::time_point animationStartTime;
 };

src/widgets/session_view.cpp
@@ -30,7 +30,10 @@ SessionView::SessionView(QWidget* parent)
         emit sourceImagesVisibleChanged(getSourceImagesVisible());
     });
 
-    connect(ui->comparisonSettings, &ComparisonSettings::settingsChanged, this, &SessionView::updateComparisonView);
+    connect(ui->comparisonSettings,
+            &ComparisonSettingsWidget::settingsChanged,
+            this,
+            &SessionView::updateComparisonView);
 
     connect(ui->emptyImage, &EmptyImage::imagesDropped, [this](QStringList const& files) {
         session->loadImages(files);
@@ -273,7 +276,7 @@ void SessionView::updateComparisonView()
     });
 
     auto const& images = session->getImages();
-    auto const& settings = *ui->comparisonSettings;
+    auto settings = ui->comparisonSettings->getComparisonSettings();
 
     bool const comparisonVisible = (images.size() >= 2);
     ui->comparisonView->setVisible(comparisonVisible);

CMakeLists.txt
@@ -26,6 +26,8 @@ add_executable(${PROJECT_NAME}
     src/busy_dialog.h
     src/command_line.cpp
     src/command_line.h
+    src/comparison_settings.cpp
+    src/comparison_settings.h
     src/comparison_view_layers.cpp
     src/comparison_view_layers.h
     src/debounce.h
@@ -44,11 +46,15 @@ add_executable(${PROJECT_NAME}
     src/screenshot_file_manager.h
     src/session.cpp
     src/session.h
+    src/slider_helpers.cpp
+    src/slider_helpers.h
     src/string_helpers.cpp
     src/string_helpers.h
-    src/widgets/comparison_settings.cpp
-    src/widgets/comparison_settings.h
-    src/widgets/comparison_settings.ui
+    src/time_helpers.cpp
+    src/time_helpers.h
+    src/widgets/comparison_settings_widget.cpp
+    src/widgets/comparison_settings_widget.h
+    src/widgets/comparison_settings_widget.ui
     src/widgets/comparison_view.cpp
     src/widgets/comparison_view.h
     src/widgets/empty_image.cpp

external/icons.qrc
@@ -12,7 +12,6 @@
         <file alias="undo.svg">mdi/svg/undo.svg</file>
         <file alias="color.svg">mdi/svg/invert-colors.svg</file>
         <file alias="clock-speed.svg">mdi/svg/clock-fast.svg</file>
-        <file alias="timer.svg">mdi/svg/timer-outline.svg</file>
         <file alias="linear.svg">mdi/svg/blur-linear.svg</file>
         <file alias="triangle-wave.svg">mdi/svg/triangle-wave.svg</file>
         <file alias="tolerance.svg">mdi/svg/approximately-equal.svg</file>
@@ -28,6 +27,7 @@
         <file alias="edit.svg">mdi/svg/pencil-outline.svg</file>
         <file alias="move-horizontal.svg">mdi/svg/arrow-left-right.svg</file>
         <file alias="move-vertical.svg">mdi/svg/arrow-up-down.svg</file>
+        <file alias="animation.svg">mdi/svg/animation-play-outline.svg</file>
     </qresource>
     <qresource prefix="/">
         <file alias="icon.svg">icon/icon.svg</file>

src/comparison_settings.cpp
@@ -1,0 +1,1 @@
+#include "comparison_settings.h"

src/comparison_settings.h
@@ -1,0 +1,58 @@
+#pragma once
+
+#include <QColor>
+
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+auto const minimumAnimationTimeBetweenImages = 100ms;
+auto const maximumAnimationTimeBetweenImages = 1000ms;
+
+enum class ComparisonMode {
+    // Compare all images, highlight the pixels where not all of them agree.
+    HighlightDifferences,
+
+    // Blend two images by laying making them half transparent and displaying them on top of each other. These modes are
+    // only available for two images.
+    BlendImagesFalseColors,
+    BlendImagesTrueColors,
+
+    // Repeatedly cycle through the images, always blending between two of them as in the true color blending mode.
+    BlendImagesAnimated,
+};
+
+struct ComparisonSettings {
+    ComparisonMode mode = ComparisonMode::HighlightDifferences;
+
+    // Additional settings for the highlight difference mode.
+    struct {
+        // Ignore minor differences below this number of gray values.
+        int tolerance = 10;
+
+        // Toggle the tolerance off or on. The tolerance value gets preserved while application is disabled.
+        bool applyTolerance = false;
+
+        // Whether to completely ignore minor differences or show them in a separate color.
+        bool showMinorDifferences = false;
+
+        // Transparency of the image layer that contains the highlighted pixels compared to the grayvalue version of the
+        // original image in the background.
+        double differencesTransparency = 0.7;
+    } highlightDifferences;
+
+    // Current blending position for the two-image blending modes (both true and false colors).
+    double blendPosition = 0.5;
+
+    // Additional settings for the false color blending mode.
+    struct {
+        QColor leftColor{255, 0, 0};
+        QColor rightColor{0, 0, 255};
+    } blendFalseColors;
+
+    // Additional settings for the animated blending mode.
+    struct {
+        std::chrono::milliseconds timeBetweenImages = 500ms;
+        bool continuous = true;
+    } animatedBlending;
+};

src/comparison_view_layers.cpp
@@ -1,13 +1,13 @@
 #include "comparison_view_layers.h"
 
+#include "comparison_settings.h"
 #include "image_difference.h"
-#include "widgets/comparison_settings.h"
 
 #include <cmath>
 
 namespace {
 
-ComparisonViewLayers computeHighlightDifferencesLayers(std::vector<Session::ImageHandle> const& images,
+ComparisonViewLayers computeHighlightDifferencesLayers(Session::Images const& images,
                                                        ComparisonSettings const& settings)
 {
     if (images.empty()) {
@@ -23,52 +23,89 @@ ComparisonViewLayers computeHighlightDifferencesLayers(std::vector<Session::Imag
         return image->image();
     });
 
+    int tolerance = 0;
+    if (settings.highlightDifferences.applyTolerance) {
+        tolerance = settings.highlightDifferences.tolerance;
+    }
+
     differenceImage =
-        computeDifferenceImage(imagesToCompare, settings.getDifferenceTolerance(), settings.showMinorDifferences());
+        computeDifferenceImage(imagesToCompare, tolerance, settings.highlightDifferences.showMinorDifferences);
 
     return {
         {images[0]->toGrayscalePixmap()},
-        {QPixmap::fromImage(differenceImage), 0.7},
+        {QPixmap::fromImage(differenceImage), settings.highlightDifferences.differencesTransparency},
     };
 }
 
-ComparisonViewLayers computeBlendLayers(std::vector<Session::ImageHandle> const& images,
-                                        ComparisonSettings const& settings)
+ComparisonViewLayers blendLayers(QPixmap const& image1, QPixmap const& image2, double blendPosition)
+{
+    return {
+        {image1, 1 - blendPosition},
+        {image2, blendPosition},
+    };
+}
+
+ComparisonViewLayers computeBlendLayers(Session::Images const& images, ComparisonSettings const& settings)
 {
     if (images.size() < 2) {
         return {};
     }
 
-    double const sequenceBlendPosition = settings.getBlendPosition() * static_cast<double>(images.size() - 1);
+    auto const& leftImage = *images[0];
+    auto const& rightImage = *images[1];
 
-    auto firstImageIndex = static_cast<size_t>(std::floor(sequenceBlendPosition));
-    firstImageIndex = std::min(firstImageIndex, images.size() - 2);
+    double const blendPosition = settings.blendPosition;
 
-    double const blendPosition = sequenceBlendPosition - firstImageIndex;
-
-    QColor firstImageColor = settings.blendImage1Color();
-    QColor secondImageColor = settings.blendImage2Color();
-    if (firstImageIndex % 2 != 0) {
-        std::swap(firstImageColor, secondImageColor);
+    QColor leftImageColor;
+    QColor rightImageColor;
+    if (settings.mode == ComparisonMode::BlendImagesFalseColors) {
+        leftImageColor = settings.blendFalseColors.leftColor;
+        rightImageColor = settings.blendFalseColors.rightColor;
     }
 
-    return {
-        {images[firstImageIndex]->toColorizedPixmap(firstImageColor), 1 - blendPosition},
-        {images[firstImageIndex + 1]->toColorizedPixmap(secondImageColor), blendPosition},
-    };
+    return blendLayers(leftImage.toColorizedPixmap(leftImageColor),
+                       rightImage.toColorizedPixmap(rightImageColor),
+                       blendPosition);
+}
+
+ComparisonViewLayers computeAnimatedBlendLayers(Session::Images const& images,
+                                                ComparisonSettings const& settings,
+                                                double animationStep)
+{
+    if (images.size() < 2) {
+        return {};
+    }
+
+    int const baseImageIndex = static_cast<int>(std::floor(animationStep));
+    size_t const firstImageIndex = baseImageIndex % images.size();
+    size_t const secondImageIndex = (firstImageIndex + 1) % images.size();
+
+    if (!settings.animatedBlending.continuous) {
+        return {
+            {images[firstImageIndex]->toPixmap()},
+        };
+    }
+
+    double const blendPosition = animationStep - baseImageIndex;
+    return blendLayers(images[firstImageIndex]->toPixmap(), images[secondImageIndex]->toPixmap(), blendPosition);
 }
 
 }  // namespace
 
-ComparisonViewLayers computeComparisonViewLayers(std::vector<Session::ImageHandle> const& images,
-                                                 ComparisonSettings const& settings)
+ComparisonViewLayers computeComparisonViewLayers(Session::Images const& images,
+                                                 ComparisonSettings const& settings,
+                                                 double animationStep)
 {
-    switch (settings.getComparisonMode()) {
+    switch (settings.mode) {
         case ComparisonMode::HighlightDifferences:
             return computeHighlightDifferencesLayers(images, settings);
-        case ComparisonMode::BlendImages:
+        case ComparisonMode::BlendImagesFalseColors:
+        case ComparisonMode::BlendImagesTrueColors:
             return computeBlendLayers(images, settings);
+        case ComparisonMode::BlendImagesAnimated:
+            return computeAnimatedBlendLayers(images, settings, animationStep);
     }
 
+    assert(false);
     return {};
 }

commit f5f70165f4c66075871f883b27509565448d63d5

Route animation updates through the session
This fixes potential crashes due to updating the mouse indicator while
also updating the comparison view. This was broken in
48f6525f9757a2c8dd51fcdaa1cb650d8d4fbd30.

src/widgets/comparison_view.cpp
@@ -12,34 +12,10 @@ ComparisonView::ComparisonView(QWidget* parent)
 {
     setCaption("Comparison View");
 
-    connect(&animationUpdateTimer, &QTimer::timeout, this, &ComparisonView::animationUpdate);
+    connect(&animationUpdateTimer, &QTimer::timeout, this, &ComparisonView::requestAnimationUpdate);
 }
 
-void ComparisonView::update(Session const& session, ComparisonSettings const& _settings)
-{
-    images = session.getImages();
-    settings = _settings;
-    update();
-
-    if (settings.mode != ComparisonMode::BlendImagesAnimated) {
-        if (animationUpdateTimer.isActive()) {
-            animationUpdateTimer.stop();
-        }
-    } else {
-        if (settings.animatedBlending.continuous) {
-            animationUpdateTimer.setInterval(continuousBlendTimerInterval);
-        } else {
-            animationUpdateTimer.setInterval(settings.animatedBlending.timeBetweenImages);
-        }
-
-        if (!animationUpdateTimer.isActive()) {
-            animationStartTime = std::chrono::steady_clock::now();
-            animationUpdateTimer.start();
-        }
-    }
-}
-
-void ComparisonView::update()
+void ComparisonView::update(Session::Images const& images, ComparisonSettings const& settings)
 {
     auto timeSinceAnimationStart = std::chrono::steady_clock::now() - animationStartTime;
     double animationStep = durationRatio(timeSinceAnimationStart, settings.animatedBlending.timeBetweenImages);
@@ -64,9 +40,26 @@ void ComparisonView::update()
 
     restoreView();
     forceViewPropagation();
+
+    updateAnimationTimerSettings(settings);
 }
 
-void ComparisonView::animationUpdate()
+void ComparisonView::updateAnimationTimerSettings(ComparisonSettings const& settings)
 {
-    update();
+    if (settings.mode != ComparisonMode::BlendImagesAnimated) {
+        if (animationUpdateTimer.isActive()) {
+            animationUpdateTimer.stop();
+        }
+    } else {
+        if (settings.animatedBlending.continuous) {
+            animationUpdateTimer.setInterval(continuousBlendTimerInterval);
+        } else {
+            animationUpdateTimer.setInterval(settings.animatedBlending.timeBetweenImages);
+        }
+
+        if (!animationUpdateTimer.isActive()) {
+            animationStartTime = std::chrono::steady_clock::now();
+            animationUpdateTimer.start();
+        }
+    }
 }

src/widgets/comparison_view.h
@@ -14,15 +14,13 @@ class ComparisonView : public ImageView {
 public:
     explicit ComparisonView(QWidget* parent = nullptr);
 
-    void update(Session const& session, ComparisonSettings const& _settings);
+    void update(Session::Images const& images, ComparisonSettings const& settings);
 
-private slots:
-    void update();
-    void animationUpdate();
+signals:
+    void requestAnimationUpdate();
 
 private:
-    Session::Images images;
-    ComparisonSettings settings;
+    void updateAnimationTimerSettings(ComparisonSettings const& settings);
 
     QTimer animationUpdateTimer;
     std::chrono::steady_clock::time_point animationStartTime;

src/widgets/session_view.cpp
@@ -34,6 +34,7 @@ SessionView::SessionView(QWidget* parent)
             &ComparisonSettingsWidget::settingsChanged,
             this,
             &SessionView::updateComparisonView);
+    connect(ui->comparisonView, &ComparisonView::requestAnimationUpdate, this, &SessionView::updateComparisonView);
 
     connect(ui->emptyImage, &EmptyImage::imagesDropped, this, [this](QStringList const& files) {
         session->loadImages(files);
@@ -287,7 +288,7 @@ void SessionView::updateComparisonView()
         return;
     }
 
-    ui->comparisonView->update(*session, settings);
+    ui->comparisonView->update(session->getImages(), settings);
 }
 
 void SessionView::clearMouseIndicators()