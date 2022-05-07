1e8a692e8c13fb7d94feb02565858818ae331773
Daniel Saier <mail@danielsaier.de>
2022-05-07 16:55:43 +0200

    Remove accidental margin around source images

--- src/widgets/source_images.cpp
+++ src/widgets/source_images.cpp
@@ -14,6 +14,7 @@ SourceImages::SourceImages(QWidget* parent)
   , layout(QBoxLayout::LeftToRight)
 {
     setLayout(&layout);
+    layout.setMargin(0);
 
     layout.addWidget(&emptyImage);
     updateEmptyImage();
